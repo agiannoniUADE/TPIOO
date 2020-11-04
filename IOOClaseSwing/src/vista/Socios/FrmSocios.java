e7cc65d6c1060467d62e6984b77b09228ce4d035
matias Caravajal <matias.caravajal@mercadolibre.com>
2020-11-04 20:10:03 -0300

    integracion con front

--- IOOClaseSwing/src/vista/Socios/FrmSocios.java
+++ IOOClaseSwing/src/vista/Socios/FrmSocios.java
@@ -1,48 +1,104 @@
 package vista.Socios;
 
+import Controllers.SocioController;
+import model.Socio;
+import model.TipoSocio;
+
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
+
+public class FrmSocios extends JFrame {
+
+    private JPanel panel1;
+    private JPanel PanelIzq;
+    private JPanel SeccBuscador;
+    private JPanel SeccABMIzq;
+    private JPanel SeccTipoyTam;
+    private JPanel SeccRSyCUIT;
+    private JPanel SeccAPyFIA;
+    private JPanel SeccDiryTel;
+    private JPanel SeccMessages;
+    private JPanel PanelDer;
+    private JPanel TabLineasDeCredito;
+    private JPanel TabContraGarantias;
+    private JPanel TabAccionistas;
+    private JTabbedPane tabbedPaneDer;
+    private JButton ButtonEditar;
+    private JTextArea textAreaMessage;
+    private JTextField textFieldEmail;
+    private JTextField TextFieldTelefono;
+    private JTextField TextFieldDireccion;
+    private JTextField TextFieldFechaInicioActividad;
+    private JTextField TextFieldActividadPrincipal;
+    private JTextField TextFieldCUIT;
+    private JTextField TextFieldRazonSocial;
+    private JComboBox comboBoxTamano;
+    private JComboBox comboBoxTipo;
+    private JButton ButtonBorrar;
+    private JButton ButtonAgregar;
+    private JButton ButtonBuscar;
+    private JTextField textBuscar;
+    private JComboBox comboBoxSeleccion;
+    private SocioController socioController;
 
 
-public class FrmSocios extends JDialog{
-  private FrmSocios self;
-  private JPanel panel1;
-  private JButton agregarButton;
-  private JButton buscarButton;
+    private FrmSocios self;
 
-  public FrmSocios(Window owner) {
-    super(owner, "");
+    public FrmSocios() throws Exception {
+        ImageIcon frameIcon = new ImageIcon("C:\\Users\\Albondigor\\Desktop\\Nullicon.png");
+        this.setIconImage(frameIcon.getImage());
 
-    this.setContentPane(panel1);
-    this.setSize(300, 300);
-    self = this;
-    //No permite volver a la pantalla anterior HASTA cerrar esta
-    this.setModal(true);
-    //Establezco el comportamiento a la hora de cerrarse
-    this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    //Que la pantalla inicie CENTRADA
-    this.setLocationRelativeTo(null);
-    buscarButton.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(final ActionEvent e) {
-
-      }
-    });
-    agregarButton.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(final ActionEvent e) {
-        FrmAgregarSocio frame = null;
+        socioController = new SocioController();
         try {
-          frame = new FrmAgregarSocio(self);
-        } catch (Exception e1) {
-          e1.printStackTrace();
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
         }
-        frame.setVisible(true);
-      }
-    });
-  }
+        this.self = this;
+        this.setContentPane(panel1);
+        this.setSize(1200,400);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setLocationRelativeTo(null);
 
+      ButtonAgregar.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          String cuit = TextFieldCUIT.getText();
+          String razonSocial = TextFieldRazonSocial.getText();
+          Date inicioActividad = new Date(TextFieldFechaInicioActividad.getText());
+          String actividadPrincipal =  TextFieldActividadPrincipal.getText();
+          TipoSocio tipoSocio = TipoSocio.lookUp(comboBoxTipo.getItemAt(comboBoxTipo.getSelectedIndex()).toString());
+          String direccion = TextFieldDireccion.getText();
+          String telefono = TextFieldTelefono.getText();
+          String email = textFieldEmail.getText();
+          String tamanioEmpresa = comboBoxTamano.getItemAt(comboBoxTamano.getSelectedIndex()).toString();
+
+          Socio nuevoSocio = new Socio(
+            cuit,
+            tipoSocio,
+            razonSocial,
+            inicioActividad,
+            actividadPrincipal,
+            direccion,
+            telefono,
+            email,
+            tamanioEmpresa);
+          try {
+            socioController.AgregarNuevoSocio(nuevoSocio);
+            textAreaMessage.append("oki doki");
+          } catch (Exception e1) {
+            e1.printStackTrace();
+          }
+        }
+      });
+    }
 
 }
