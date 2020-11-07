7aa4756efd49d843fba46a818b27b13585627dc9
agiannoni12 <63304513+agiannoni12@users.noreply.github.com>
2020-11-07 10:48:15 -0300

    se actualiza TipoDocumentoRegistro

--- IOOClaseSwing/src/model/TipoDocumentoRegistro.java
+++ IOOClaseSwing/src/model/TipoDocumentoRegistro.java
@@ -42,4 +42,17 @@ public class  TipoDocumentoRegistro {
   public void setNombre(String nombre) {
     this.nombre = nombre;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    TipoDocumentoRegistro that = (TipoDocumentoRegistro) o;
+    return id == that.id;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 }
