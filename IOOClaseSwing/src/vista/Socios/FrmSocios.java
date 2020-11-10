4cc2d9552d686f7bccc603898b5224b455994e90
SilvestreCasagrande <41748381+SilvestreCasagrande@users.noreply.github.com>
2020-11-10 01:05:42 -0300

    botones dinamicos

--- IOOClaseSwing/src/vista/Socios/FrmSocios.java
+++ IOOClaseSwing/src/vista/Socios/FrmSocios.java
@@ -3,10 +3,15 @@ package vista.Socios;
 import Controllers.SocioController;
 import model.Socio;
 import model.TipoSocio;
+import vista.State;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.Date;
 
 public class FrmSocios extends JFrame {
@@ -24,7 +29,7 @@ public class FrmSocios extends JFrame {
     private JPanel TabContraGarantias;
     private JPanel TabAccionistas;
     private JTabbedPane tabbedPaneDer;
-    private JButton ButtonEditar;
+    private JButton ButtonBorrar;
     private JTextArea textAreaMessage;
     private JTextField textFieldEmail;
     private JTextField TextFieldTelefono;
@@ -35,15 +40,16 @@ public class FrmSocios extends JFrame {
     private JTextField TextFieldRazonSocial;
     private JComboBox comboBoxTamano;
     private JComboBox comboBoxTipo;
-    private JButton ButtonBorrar;
-    private JButton ButtonAgregar;
+    private JButton ButtonEditarConfirmar;
+    private JButton ButtonAgregarCancelar;
     private JButton ButtonBuscar;
     private JTextField textBuscar;
   private JButton agregarButton;
   private JButton borrarButton;
   private JButton editarButton;
+  private JLabel StateLabel;
   private SocioController socioController;
-
+  private State State;
 
     private FrmSocios self;
 
@@ -51,6 +57,7 @@ public class FrmSocios extends JFrame {
         ImageIcon frameIcon = new ImageIcon("C:\\Users\\Albondigor\\Desktop\\Nullicon.png");
         this.setIconImage(frameIcon.getImage());
 
+        State = new State();
         socioController = new SocioController();
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -69,7 +76,7 @@ public class FrmSocios extends JFrame {
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setLocationRelativeTo(null);
 
-      ButtonAgregar.addActionListener(new ActionListener() {
+      ButtonAgregarCancelar.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
           String cuit = TextFieldCUIT.getText();
@@ -104,24 +111,117 @@ public class FrmSocios extends JFrame {
         @Override
         public void actionPerformed(ActionEvent e) {
           try {
-            Socio s = socioController.getSocioParticipe(Integer.parseInt(textBuscar.getText()));
+            if (State.getCurrent() == "Buscado") {
+              ButtonBuscar.setText("Buscar");
+              textBuscar.setEnabled(true);
+              State.standby();
+            } else {
+              Socio s = socioController.getSocioParticipe(Integer.parseInt(textBuscar.getText()));
 
-            TextFieldCUIT.setText(s.cuit);
-            TextFieldRazonSocial.setText(s.razonSocial);
-            TextFieldFechaInicioActividad.setText(s.fechaInicioActividad.toString());
-            TextFieldActividadPrincipal.setText(s.actividadPrincipal);
-            TextFieldDireccion.setText(s.direccion);
-            TextFieldTelefono.setText(s.telefono);
-            textFieldEmail.setText(s.email);
+              TextFieldCUIT.setText(s.cuit);
+              TextFieldRazonSocial.setText(s.razonSocial);
+              TextFieldFechaInicioActividad.setText(s.fechaInicioActividad.toString());
+              TextFieldActividadPrincipal.setText(s.actividadPrincipal);
+              TextFieldDireccion.setText(s.direccion);
+              TextFieldTelefono.setText(s.telefono);
+              textFieldEmail.setText(s.email);
 
-            textBuscar.setEnabled(false);
-            ButtonBuscar.setText("Nueva busqueda");
-
+              textBuscar.setEnabled(false);
+              State.setCurrent("Buscado");
+              State.setEnv("Socios");
+              ButtonBuscar.setText("Nueva busqueda");
+            }
           } catch (Exception e1) {
             e1.printStackTrace();
           }
         }
       });
-    }
+      ButtonBorrar.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          try {
+          if (State.getEnv() == "Socios") {
+            if (State.getCurrent() == "Editando") {
+              //
+            } else if (State.getCurrent() == "Borrando") {
+              //
+            }
+            ButtonEditarConfirmar.setText("Editar");
+            ButtonAgregarCancelar.setText("Agregar");
+            ButtonBorrar.setVisible(true);
+            State.standby();
+          }
+        } catch (Exception e1) {
+          e1.printStackTrace();
+        }
+        }
+        });
+      ButtonEditarConfirmar.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          try {
+            State.setCurrent("Editando");
+            State.setEnv("Socios");
+            StateLabel.setText(State.getCurrent());
+            ButtonEditarConfirmar.setText("Confirmar");
+            ButtonAgregarCancelar.setText("Cancelar");
+            ButtonBorrar.setVisible(false);
 
-}
+          } catch (Exception e1) {
+            e1.printStackTrace();
+          }
+
+      }});
+      ButtonAgregarCancelar.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          try {
+          if (State.getCurrent() == "Standby") {
+            State.setCurrent("Agregando");
+            State.setEnv("Socios");
+            StateLabel.setText(State.getCurrent());
+            ButtonEditarConfirmar.setText("Confirmar");
+            ButtonAgregarCancelar.setText("Cancelar");
+            ButtonBorrar.setVisible(false);
+            TextFieldCUIT.setText("");
+            TextFieldRazonSocial.setText("");
+            TextFieldFechaInicioActividad.setText("");
+            TextFieldActividadPrincipal.setText("");
+            TextFieldDireccion.setText("");
+            TextFieldTelefono.setText("");
+            textFieldEmail.setText("");
+
+          } else if (State.getEnv() == "Socios") {
+              if (State.getCurrent() == "Editando") {
+                //
+              } else if (State.getCurrent() == "Borrando") {
+                //
+              }
+              ButtonEditarConfirmar.setText("Editar");
+              ButtonAgregarCancelar.setText("Agregar");
+              ButtonBorrar.setVisible(true);
+              State.standby();
+            }
+
+          } catch (Exception e1) {
+            e1.printStackTrace();
+          }
+          }
+
+      });
+
+
+      StateLabel.addComponentListener(new ComponentAdapter() {
+        @Override
+        public void componentResized(ComponentEvent e) {
+          super.componentResized(e);
+          if (State.getCurrent() == "Standby") {
+            StateLabel.setVisible(false);
+          } else {
+            StateLabel.setText(State.getCurrent());
+            StateLabel.setVisible(true);
+          }
+        }
+      });
+    }};
+
