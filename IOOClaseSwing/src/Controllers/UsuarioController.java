122af5ab09db58b7707f9bbc3f455c838620ebbc
agiannoni12 <63304513+agiannoni12@users.noreply.github.com>
2020-11-11 21:10:32 -0300

    se agrgan las clases de usuario

--- IOOClaseSwing/src/Controllers/UsuarioController.java
+++ IOOClaseSwing/src/Controllers/UsuarioController.java
@@ -1,54 +1,73 @@
 package Controllers;
-import DAO.GenericDAO;
-import DAO.UsuariosDAO;
+import DAO.UsuarioDao;
 import model.*;
 
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.List;
 
-public class UsuariosController {
+public class UsuarioController {
 
-    private UsuariosDAO usuariosDAO;
+    private UsuarioDao usuarioDao;
 
-    public UsuariosController() throws Exception {
-         this.usuariosDAO = new UsuariosDAO();
+    /**
+     * Default constructor
+     */
+
+    public UsuarioController() throws Exception {
+         this.usuarioDao = new UsuarioDao();
 
     }
 
 
 
     public int AgregarNuevoUsuario(Usuario usuario) throws Exception {
-        GenericDAO dao = usuario.getUsuario(usuario);
 
+        int lastId= usuarioDao.getLastInsertId();
+        lastId++;
+        usuario.setId(lastId);
+        usuarioDao.save(usuario);
 
-        int lastId = dao.getLastInsertId();
-            lastId++;
-            usuario.setId(lastId);
-            usuariosDAO.save(usuario);
         return lastId;
 
 
         }
 
-    public Usuario getUsuario(int id) throws FileNotFoundException {
-        Object obj = usuariosDAO.search(id);
+    public Usuario getUsuario (int id) throws FileNotFoundException {
+        Object obj = usuarioDao.search(id);
         return obj != null ? (Usuario)obj: null;
     }
 
 
-    public Boolean updateSocio(Usuario usuario) throws Exception {
+    public Boolean updateUsuario (Usuario usuario) throws Exception {
 
-        return usuariosDAO.update(usuario);
+        return usuarioDao.update(usuario);
     }
 
     /**
      * @param id
      */
     public Boolean delete(int id) throws Exception {
-        return usuariosDAO.delete(id);
+        return usuarioDao.delete(id);
     }
 
 
+    public int getLastInsertId() throws Exception {
+        return usuarioDao.getLastInsertId();
+    }
 
+    public List<Usuario> getUsuarios() throws Exception {
+        List<Usuario> lista = usuarioDao.getAll();
+        return lista;
+    }
 
+/*
+    public Boolean verificarIngreso(String palabra){
+        if (palabra.equals(this.password)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+*/
 }
